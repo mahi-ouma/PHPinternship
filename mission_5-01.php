ae8ebe60b55981c0240ee9da03a87dc0086acba5
mahiro.baba <v.v.mahiro1390@gmail.com>
2021-03-22 18:41:20 +0900

    commit

--- mission_5-01.php
+++ mission_5-01.php
@@ -1,7 +1,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>mahi's billboard</title>
+    <title>billboard</title>
 </head>
 <body>
     <form action="" method="post" id="submit">
