86451ae71960669a68ca4b7f62c2d1a70b06add3
mahiro.baba <v.v.mahiro1390@gmail.com>
2021-03-22 19:04:49 +0900

    データベース変更

--- mission_5-01.php
+++ mission_5-01.php
@@ -34,7 +34,7 @@
     <br>
     <?php
         // DB接続設定
-        $dsn = 'データベース名';
+        $dsn = 'データベース';
         $user = 'ユーザー名';
         $password = 'パスワード';
         $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
